099c2a6e39410da7b667d383d719d0ff044547de
Nazianaseer77 <160523698+Nazianaseer77@users.noreply.github.com>
2024-03-20 22:10:19 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,17 @@
+// let weightInKg = 70;  //70kg
+// let heightInMeters =1.75;  //1.75m
+// let bmi = weightInKg /(heightInMeters*heightInMeters);
+// console.log(`yourBMI is ${bmi}`);
+
+
+
+
+
+
+
+
+let a: number = 5;
+let b: number = 2;
+let c: number;
+c = ++a + --b + a + b++ +b;
+console.log(c)
